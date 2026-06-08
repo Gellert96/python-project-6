2e6e7234840ea57f57f70663ae03911e1947a27e
Gregory <gregory@example.com>
2026-06-08 12:53:22 +0300

    fix category and product logic

--- src/category.py
+++ src/category.py
@@ -1,11 +1,28 @@
+from src.product import Product
+
+
 class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products=None):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
 
         Category.category_count += 1
-        Category.product_count += len(products)
+
+        if products:
+            for product in products:
+                self.add_product(product)
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        return [
+            f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт."
+            for p in self.__products
+        ]
