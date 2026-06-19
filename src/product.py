589ff14d43a1572442a6632ee8e77a201e521f1a
Gregory <gregory@example.com>
2026-06-19 16:06:11 +0400

    Добавлена инкапсуляция цены продукта

--- src/product.py
+++ src/product.py
@@ -2,5 +2,16 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
